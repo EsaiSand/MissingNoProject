1c4761f60e2c2a3c446d81471df7a508c1c7be02
RomanB1253 <161875451+RomanB1253@users.noreply.github.com>
2024-03-15 10:47:00 -0700

    Update main.py
    added includes and main function

---
+++ main/main.py
@@ -0,0 +1,10 @@
+include user
+include expenses
+include ingredients
+include meals
+include expenses
+include subcriptions
+include debt
+
+main():
+  '''Main function of the program. What runs the full code'''
