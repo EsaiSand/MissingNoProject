aa3f9348e96ed1510f7c5984f5812c59ea70e1c2
Luisnav03 <161875387+Luisnav03@users.noreply.github.com>
2024-03-15 11:52:58 -0700

    Update expense.py

---
+++ classes/expense.py
@@ -0,0 +1,31 @@
+class expense:
+    '''Attributes:
+        name: str
+        cost: float
+        date: str
+        category: str
+
+    '''
+
+
+
+def expense(self):
+    "create the user object"
+    self.name = ''
+    self.cost = ''
+    self.date = ''
+    self.catergory = ''
+
+
+
+def get_name(self, name):
+    '''set the name of the expense and store it'''
+
+def get_cost(self, cost):
+    '''set the cost of the expense'''
+
+def get_date(self, date):
+    '''set the date of the expense and store it'''
+
+def get_category(self,category):
+    '''Set the expense in a categgory'''
