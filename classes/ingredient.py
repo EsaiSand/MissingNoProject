a98e3d80ed7b2244c591befac7a21eaddac24d07
RomanB1253 <161875451+RomanB1253@users.noreply.github.com>
2024-04-19 00:30:49 -0700

    Meals/Ingredients
    Added functionality to both the meal and ingredient classes

--- classes/ingredient.py
+++ classes/ingredient.py
@@ -6,7 +6,7 @@ class Ingredients:
         calories: int
         categories: str
     '''
-def ingredients(self):
+def __init__(self):
     '''Creates the ingredient object'''
     self.ingredient_name = ''
     self.ingredient_price = 0
@@ -15,9 +15,40 @@ def ingredients(self):
 
 def get_calories(self):
     '''gets the calories of the ingredient'''
+    valid = False
+    while valid != True:
+        try:
+            self.calories = int(input())
+            if self.calories < 0:
+                raise ValueError
+            
+        except ValueError:
+            print("value must be an integer greater than 0")
+        else:
+            valid = True
+
+    return self.calories
 
 def get_ingredient_name(self):
     '''gets the name of the ingredient'''
+    self.ingredient_name = input("input the name of the ingredient: ")
+    return self.ingredient_name
+
 
 def get_ingredient_price(self):
     '''gets the price of the ingredients'''
+    valid = False
+    while valid != True:
+        try:
+            self.ingredient_price = float(input("input the price of the ingredient: "))
+            if self.ingredient_price < 0:
+                raise ValueError
+        except ValueError:
+            print("value must be an integer greater than 0")
+        else:
+            valid = True
+
+    return self.ingredient_price
+
+def get_food_catagories(self):
+    self.catagory = input("what nutritiona catagory is this item?: ").lower
--- classes/meal.py
+++ classes/meal.py
@@ -1,23 +1,38 @@
+from ingredient import Ingredients
+
 class Meal:
     '''
     Attributes:
         food: str
         price: float
-        ingredients: str
+        ingredients: list
     '''
-def meal(self):
+def __init__(self):
     '''Creates the meal object'''
     self.food = ''
     self.price = 0
-    self.ingredients = ''
-def set_food_name(self, food, price):
+    self.ingredients = []
+
+def set_food_name(self):
     '''Gives the meal object a price based on ingredients used in the meal'''
+    self.food = input("What is the name of this meal?: ")
+    return self.food
 
-def set_price(self, price):
+def set_price(self):
     '''sets the price of the meals'''
+    for i in len(self.ingredients):
+        self.price += self.ingredients[i].ingredient_price
+    return self.price
 
-def get_ingredients(self):
-    '''gets the ingredients for the meal'''
+def add_ingredients(self, ingredient):
+    '''adds the ingredient to a list that makes up the meal'''
+    self.ingredients.append(ingredient)
+
+def remove_ingredients(self, ingredient):
+    '''removes the last ingredient from the meal '''
+    self.ingredients.pop()
 
 def list_ingredients(self):
-    '''grants and displays a list of ingredients of the meal object'''
+    '''displays a list of ingredients of the meal object'''
+    for i in range(len(self.ingredients)):
+        print(f'- {self.ingredients[1]}')
