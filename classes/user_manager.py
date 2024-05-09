1684b96c35d745c1d82e5f93e1af42c5d1eb4849
RomanB1253 <161875451+RomanB1253@users.noreply.github.com>
2024-05-09 14:00:38 -0700

    Update user_manager.py
    Added user_storage and get_user methods (effectively to/from json)

--- classes/user_manager.py
+++ classes/user_manager.py
@@ -5,9 +5,11 @@ from meal import Meal
 from subscription import Subscription
 from user import User
 import helpers as help
+import json
 
 def main():
   UserManager.startup()
+
 class UserManager:
   '''User manager represents a user acccount:
   It controls the connection between a User object and its
@@ -20,7 +22,7 @@ class UserManager:
     self.expenses = []
     self.subcriptions = []
     self.meals = []
-    self.ingredients = [] 
+    self.ingredients = []
 
   @staticmethod
   def startup():
@@ -41,6 +43,58 @@ class UserManager:
     else:
       pass
 
+  def user_storage(self):
+    '''Stores all user data (from each class) into a single file'''
+
+    #loops store all data object elements into a json list, which is stored
+    debts_json = []
+    for i in range(len(self.debts)):
+      debts_json[i].append(self.debt[i].to_json())
+
+    expenses_json = []
+    for i in range(len(self.expenses)):
+      expenses_json[i].append(self.expenses[i].to_json())
+
+    subscriptions_json = []
+    for i in range(len(self.subscriptions)):
+      subscriptions_json[i].append(self.expenses[i].to_json())
+
+    meals_json = []
+    for i in range(len(self.meals)):
+      meals_json[i].append(self.meals[i].to_json())
+
+    ingredients_json = []
+    for i in range(len(self.ingredients)):
+      ingredients_json[i].append(self.ingredients[i].to_json())
+
+    #creates a dictionary of all the json strings and stores them in a dictionary
+    attr_dict = {
+      "User": self.user.to_json(),
+      "Debts": debts_json,
+      "Expenses": expenses_json,
+      "Subscriptions": subscriptions_json,
+      "Meals": meals_json,
+      "Ingredients": ingredients_json
+    }
+    json_format = json.dumps(attr_dict, indent=4)
+    return json_format
+
+  @staticmethod
+  def get_user(json_string):
+    '''grabs elements from json and and constructs/returns a usermanager object'''
+    
+    attr_dict = json.loads(json_string)
+
+    new_userman = UserManager()
+
+    new_userman.user = attr_dict["User"]
+    new_userman.debts = attr_dict["Debts"]
+    new_userman.expenses = attr_dict["Expenses"]
+    new_userman.subcriptions = attr_dict["Subscriptions"]
+    new_userman.meals = attr_dict["Meals"]
+    new_userman.ingredients = attr_dict["Ingredients"]
+
+    return new_userman
 
 if __name__ == "__main__":
     main()
